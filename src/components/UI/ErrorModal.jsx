43dc9e764f33325a7c79a667b907d36219b4b5ae
smoak88 <smoak88@gmail.com>
2021-11-19 15:08:05 -0800

    Add Error Modal and Error State

--- src/components/UI/ErrorModal.jsx
+++ src/components/UI/ErrorModal.jsx
@@ -3,17 +3,67 @@ import styled from "styled-components";
 import Card from "./Card";
 import Button from "./Button";
 
+const ModalCard = styled(Card)`
+  position: fixed;
+  top: 30vh;
+  left: 10%;
+  width: 80%;
+  z-index: 100;
+  overflow: hidden;
+  padding: 0;
+
+  @media (min-width: 768px) {
+    left: calc(50% - 20rem);
+    width: 40rem;
+  }
+
+  & header {
+    background: #4f005f;
+    padding: 1rem;
+  }
+
+  & header h2 {
+    margin: 0;
+    color: white;
+  }
+
+  & div {
+    padding: 1rem;
+  }
+
+  & footer {
+    padding: 1rem;
+    display: flex;
+    justify-content: flex-end;
+  }
+`;
+
+const BackdropDiv = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100vh;
+  z-index: 10;
+  background: rgba(0, 0, 0, 0.75);
+`;
+
 const ErrorModal = (props) => {
   return (
-    <Card>
-      <header>
-        <h2></h2>
-      </header>
-      <div>
-        <p></p>
-      </div>
-      <footer></footer>
-    </Card>
+    <>
+      <BackdropDiv onClick={props.onConfirm} />
+      <ModalCard>
+        <header>
+          <h2> {props.title} </h2>
+        </header>
+        <div>
+          <p>{props.message} </p>
+        </div>
+        <footer>
+          <Button onClick={props.onConfirm}>Okay</Button>
+        </footer>
+      </ModalCard>
+    </>
   );
 };
 
--- src/components/Users/AddUser.jsx
+++ src/components/Users/AddUser.jsx
@@ -1,17 +1,27 @@
 import Card from "../UI/Card";
 import Button from "../UI/Button";
 import { useState } from "react";
+import ErrorModal from "../UI/ErrorModal";
 
 const AddUser = (props) => {
   const [enteredUsername, setEnteredUsername] = useState("");
   const [enteredAge, setEnteredAge] = useState("");
+  const [error, setError] = useState();
 
   const addUserHandler = (event) => {
     event.preventDefault();
     if (enteredUsername.trim().length === 0 || enteredAge.trim().length === 0) {
+      setError({
+        title: "Invalid Input",
+        message: "Please enter a valid name and age (non-empty values).",
+      });
       return;
     }
     if (+enteredAge < 1) {
+      setError({
+        title: "Invalid age",
+        message: "Please enter a valid age (>0).",
+      });
       return;
     }
 
@@ -31,27 +41,40 @@ const AddUser = (props) => {
     setEnteredAge(age);
   };
 
+  const errorHandler = () => {
+    setError(null);
+  };
+
   return (
-    <Card>
-      <form onSubmit={addUserHandler}>
-        <label htmlFor="username">Username</label>
-        <input
-          id="username"
-          type="text"
-          onChange={usernameChangeHandler}
-          value={enteredUsername}
+    <>
+      {error && (
+        <ErrorModal
+          onConfirm={errorHandler}
+          title={error.title}
+          message={error.message}
         />
-        {/* htmlFor hooks up with the input id to help with screen readers and stuff */}
-        <label htmlFor="age">Age (Years)</label>
-        <input
-          id="age"
-          type="number"
-          onChange={ageChangeHandler}
-          value={enteredAge}
-        />
-        <Button type="submit">Add User</Button>
-      </form>
-    </Card>
+      )}
+      <Card>
+        <form onSubmit={addUserHandler}>
+          <label htmlFor="username">Username</label>
+          <input
+            id="username"
+            type="text"
+            onChange={usernameChangeHandler}
+            value={enteredUsername}
+          />
+          {/* htmlFor hooks up with the input id to help with screen readers and stuff */}
+          <label htmlFor="age">Age (Years)</label>
+          <input
+            id="age"
+            type="number"
+            onChange={ageChangeHandler}
+            value={enteredAge}
+          />
+          <Button type="submit">Add User</Button>
+        </form>
+      </Card>
+    </>
   );
 };
 

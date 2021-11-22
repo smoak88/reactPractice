c3cb76a942ac05623015514cd25ee235dbeaeb01
smoak88 <smoak88@gmail.com>
2021-11-22 11:03:05 -0800

    Add portal for Error Modal and Backdrop

--- src/components/UI/ErrorModal.jsx
+++ src/components/UI/ErrorModal.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import styled from "styled-components";
 import Card from "./Card";
 import Button from "./Button";
+import ReactDOM from "react-dom";
 
 const ModalCard = styled(Card)`
   position: fixed;
@@ -48,21 +49,41 @@ const BackdropDiv = styled.div`
   background: rgba(0, 0, 0, 0.75);
 `;
 
+const Backdrop = (props) => {
+  return <BackdropDiv onClick={props.onConfirm} />;
+};
+
+const ModalOverlay = (props) => {
+  return (
+    <ModalCard>
+      <header>
+        <h2> {props.title} </h2>
+      </header>
+      <div>
+        <p>{props.message} </p>
+      </div>
+      <footer>
+        <Button onClick={props.onConfirm}>Okay</Button>
+      </footer>
+    </ModalCard>
+  );
+};
+
 const ErrorModal = (props) => {
   return (
     <>
-      <BackdropDiv onClick={props.onConfirm} />
-      <ModalCard>
-        <header>
-          <h2> {props.title} </h2>
-        </header>
-        <div>
-          <p>{props.message} </p>
-        </div>
-        <footer>
-          <Button onClick={props.onConfirm}>Okay</Button>
-        </footer>
-      </ModalCard>
+      {ReactDOM.createPortal(
+        <Backdrop onConfirm={props.onConfirm} />,
+        document.getElementById("backdrop-root")
+      )}
+      {ReactDOM.createPortal(
+        <ModalOverlay
+          title={props.title}
+          message={props.message}
+          onConfirm={props.onConfirm}
+        />,
+        document.getElementById("overlay-root")
+      )}
     </>
   );
 };
